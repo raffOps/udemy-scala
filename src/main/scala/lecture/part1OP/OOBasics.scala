83d3aadcf961484fe0d07cc0993250dc813ca989
Rafael Júnior Ribeiro <rafael.ribeiro@contaquanto.com.br>
2022-07-31 17:34:31 -0300

    added oo basics

--- src/main/scala/lecture/part1OP/OOBasics.scala
+++ src/main/scala/lecture/part1OP/OOBasics.scala
@@ -3,8 +3,31 @@ package lecture.part1OP
 
 class Person(val name: String, val age: Int) {
 
+  def this(name: String) = this(name, 0) // useless
+
   def greet(name: String): Unit = {
-    println(s"$this.name says: Hi $name")
+    println(s"${this.name} says: Hi $name")
+  }
+
+  def greet(): Unit = {
+    println(s"Hi, my name is ${this.name}")
+  }
+}
+
+
+class Writer(val firstName: String, val surname: String, val year: Int) {
+  def getFullName: String = {
+    s"${this.firstName} ${this.surname}"
+  }
+}
+
+class Novel(val title: String, val year: Int, val writer: Writer) {
+  def getWriterAge: Int = {
+    writer.year
+  }
+
+  def getWriter: String = {
+    writer.getFullName
   }
 }
 
@@ -12,4 +35,5 @@ object OOBasics extends App {
   val person = new Person(name = "Rafa", age = 21)
   println(person.age)
   person.greet(name = "Thiago")
+  person.greet()
 }
